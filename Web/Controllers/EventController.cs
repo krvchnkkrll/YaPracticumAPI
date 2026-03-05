9abe073416e8b74398234261376a51766b1f58fb
krvchnkkrll <krvchnkkrll@gmail.com>
2026-03-06 02:32:02 +0300

    Добавил документацию в контроллер

--- Web/Controllers/EventController.cs
+++ Web/Controllers/EventController.cs
@@ -7,6 +7,9 @@ namespace Web.Controllers;
 [Route("api/events")]
 public sealed class EventController(IEventService eventService) : AppController
 {
+    /// <summary>
+    ///     Получить все события
+    /// </summary>
     [HttpGet]
     [ProducesResponseType(typeof(IEnumerable<GetEventResponse>), StatusCodes.Status200OK)]
     public ActionResult<IEnumerable<GetEventResponse>> Get()
@@ -14,6 +17,11 @@ public sealed class EventController(IEventService eventService) : AppController
         return Ok(eventService.GetEvents());
     }
 
+    /// <summary>
+    ///     Получить событие
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     [HttpGet("{id}")]
     [ProducesResponseType(typeof(GetEventResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -22,13 +30,21 @@ public sealed class EventController(IEventService eventService) : AppController
         return Ok(eventService.GetEvent(id));
     }
 
+    /// <summary>
+    ///     Создать событие
+    /// </summary>
+    /// <param name="createEventRequest"></param>
+    /// <returns></returns>
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
     public ActionResult<GetEventResponse> Post([FromBody] CreateEventRequest createEventRequest)
     {
         return Ok(eventService.CreateEvent(createEventRequest));
     }
-
+    
+    /// <summary>
+    ///     Изменить событие
+    /// </summary>
     [HttpPut("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -37,6 +53,9 @@ public sealed class EventController(IEventService eventService) : AppController
         return Ok(eventService.UpdateEvent(id, updateEventRequest));
     }
 
+    /// <summary>
+    ///     Удалить событие
+    /// </summary>
     [HttpDelete("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
