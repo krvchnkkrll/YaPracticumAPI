20d9212be7225c7cb5a7fa3838d69e7a56bc5c91
krvchnkkrll <krvchnkkrll@gmail.com>
2026-04-02 00:52:18 +0300

    Добавил документацию

--- Application.Contracts/Models/GetEvents/GetEventsResponse.cs
+++ Application.Contracts/Models/GetEvents/GetEventsResponse.cs
@@ -2,5 +2,8 @@ namespace Application.Contracts.Models.GetEvents;
 
 public sealed class GetEventsResponse
 {
+    /// <summary>
+    ///     Список событий
+    /// </summary>
     public required IEnumerable<GetEventResponse> Events { get; init; }
 }
--- Application.Contracts/Models/GetEvents/GetEventsSearchQuery.cs
+++ Application.Contracts/Models/GetEvents/GetEventsSearchQuery.cs
@@ -2,7 +2,18 @@ namespace Application.Contracts.Models.GetEvents;
 
 public sealed class GetEventsSearchQuery
 {
+    /// <summary>
+    ///     Название события
+    /// </summary>
     public required string? Title { get; init; }
+    
+    /// <summary>
+    ///     Начинаются с
+    /// </summary>
     public required DateTime? From { get; init; }
+    
+    /// <summary>
+    ///     Заканчиваются не позднее
+    /// </summary>
     public required DateTime? To { get; init; }
 }
