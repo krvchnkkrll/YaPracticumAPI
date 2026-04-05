d8bfe814b8c5cad57b8a6de81036bf96b1125705
krvchnkkrll <krvchnkkrll@gmail.com>
2026-04-05 17:36:18 +0300

    Добавляет регистрацию в DI

--- Persistence/DependencyInjection.cs
+++ Persistence/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Persistence.Contracts.Repositories;
+using Persistence.Contracts.Storages;
 using Persistence.Repositories;
 
 namespace Persistence;
@@ -10,6 +11,6 @@ public static class DependencyInjection
     public static void AddPersistence(this IHostApplicationBuilder builder)
     {
         builder.Services.AddScoped<IEventRepository, EventRepository>();
-        builder.Services.AddSingleton<EventStorage>();
+        builder.Services.AddSingleton<IEventStorage, EventStorage>();
     }
 }
