220b9c8aa79ebfa5da9bef38e7b3f792c6de072a
krvchnkkrll <krvchnkkrll@gmail.com>
2026-04-05 17:42:41 +0300

    Убирает required с фильтров

--- Application.Contracts/Models/GetEventsSearchQuery.cs
+++ Application.Contracts/Models/GetEventsSearchQuery.cs
@@ -5,15 +5,15 @@ public sealed class GetEventsSearchQuery
     /// <summary>
     ///     Название события
     /// </summary>
-    public required string? Title { get; init; }
+    public string? Title { get; init; }
     
     /// <summary>
     ///     Начинаются с
     /// </summary>
-    public required DateTime? From { get; init; }
+    public DateTime? From { get; init; }
     
     /// <summary>
     ///     Заканчиваются не позднее
     /// </summary>
-    public required DateTime? To { get; init; }
+    public DateTime? To { get; init; }
 }
