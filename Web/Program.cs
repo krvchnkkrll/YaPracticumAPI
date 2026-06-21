a3ca76fb4aee2ecb572f18c10753914986a94083
krvchnkkrll <krvchnkkrll@gmail.com>
2026-06-21 21:56:42 +0300

    Миграция InitialCreate. Удаляет создание БД при запуске приложения.

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,5 @@
 using Application;
 using Persistence;
-using Persistence.Contracts;
 using Web;
 using Web.Common.Middleware;
 
@@ -11,12 +10,6 @@ builder.AddPersistence();
 builder.AddWeb();
 
 var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<IDbContext>();
-    db.Database.EnsureCreated();
-}
 
 if (app.Environment.IsDevelopment())
 {
