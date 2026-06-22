f9dcf7067d1d4670307ec3611e9b14b2ff927b28
krvchnkkrll <krvchnkkrll@gmail.com>
2026-06-23 01:45:13 +0300

    Добавляет автомигратор

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,5 +1,7 @@
 using Application;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
+using Persistence.Contracts;
 using Web;
 using Web.Common.Middleware;
 
@@ -15,6 +17,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<IDbContext>();
+    await db.Database.MigrateAsync();
 }
 
 app.UseHttpsRedirection();
