69672091859312dc459c4cb8874df4d107f422c3
krvchnkkrll <krvchnkkrll@gmail.com>
2026-07-21 01:12:37 +0300

    Миграция сервиса событий

---
+++ Events.Infrastructure/Migrations/20260720221227_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Events.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "events",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    description = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: true),
+                    start_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    total_seats = table.Column<int>(type: "integer", nullable: false),
+                    available_seats = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_events", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events");
+        }
+    }
+}
