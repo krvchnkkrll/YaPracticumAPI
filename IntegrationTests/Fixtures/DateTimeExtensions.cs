7efa7a99fdd6589b3997ac5f0634683dae7f1808
krvchnkkrll <krvchnkkrll@gmail.com>
2026-07-24 01:25:13 +0300

    Удаляет монолитные тесты

--- IntegrationTests/Fixtures/DateTimeExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace IntegrationTests.Fixtures;
-
-internal static class DateTimeExtensions
-{
-    public static DateTime TruncateToMicroseconds(this DateTime value)
-        => new(value.Ticks - value.Ticks % TimeSpan.TicksPerMicrosecond, value.Kind);
-}
